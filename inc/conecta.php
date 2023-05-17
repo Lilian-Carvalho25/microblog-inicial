085941f3ab0af017329ccd06210071b4c4e5662c
Lilian Carvalho Santos Ribeiro <kleidk74@gmail.com>
2023-05-17 17:31:42 -0300

    Integrando o banco de dados

---
+++ inc/conecta.php
@@ -0,0 +1,22 @@
+<?php 
+
+/* Parâmetros de acesso ao servidor de banco de dados MySQL */
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "microblog-inicial";
+
+/* Usando a função mysqli_connect para conectar ao servidor */
+$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+/* Definindo o charset UTF8 também para a comunicação com o banco de dados */
+mysqli_set_charset($conexao, "utf8");
+
+/* Se der algum problema de conexão (ou resultado, conexao false) pare tudo (die) e mostre o erro. */
+if( !$conexao ){
+    die(mysqli_connect_error($conexao));
+/* Senão, beijos e abraços */
+} else {
+    echo "<p>Beleza, banco conectado!</p>";
+}
+?>
